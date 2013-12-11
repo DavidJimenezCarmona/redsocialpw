07fa10153fefa8d33543d7874f7de93d51f38a6f
tatyperson22@gmail.com <tatyperson22@gmail.com@15a9a433-5e3c-3676-0093-ef6a8a42323a>
2013-12-11 14:22:11 +0000

    <? por <?php

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -3,9 +3,9 @@
 <br>
 
 <div id="cajalog">
-	<? echo form_open('welcome/login'); ?>
+	<?php echo form_open('welcome/login'); ?>
 	<p>Usuario:<br> <?= form_input('usuario'); ?><br>
 	<br>Contraseña:<br> <?= form_password('pass'); ?></p>
-	<? echo form_submit('Entrar', 'Entrar'); ?><br>
+	<?php echo form_submit('Entrar', 'Entrar'); ?><br>
 	<a href=/redsocialpw/>¿Has olvidado tu contraseña?</a>
 </div>
