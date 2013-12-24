5cf3bcaffb7752c06d116e5b58c6e667cacde712
tatyperson22@gmail.com <tatyperson22@gmail.com@15a9a433-5e3c-3676-0093-ef6a8a42323a>
2013-12-24 14:42:09 +0000

    Lavaito de cara :D

--- application/views/headers.php
+++ application/views/headers.php
@@ -3,6 +3,8 @@
 	<head>
 		<meta charset="utf-8">
 		<link rel="stylesheet" type="text/css" href="/redsocialpw/css/estilos.css">
+		<link href='http://fonts.googleapis.com/css?family=Bowlby+One+SC' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Chau+Philomene+One' rel='stylesheet' type='text/css'>
 		<title>Red eventos</title>
 	</head>
 	<body>
--- application/views/inicio.php
+++ application/views/inicio.php
@@ -1,11 +1,13 @@
 <br>
-<h1>Bienvenido a tu red de eventos</h1>
+<h1>FriendShipDiary</h1>
+<img id="logo" src="img/logo2.png"/>
 <br>
 
 <div id="cajalog">
 	<? echo form_open('welcome/login'); ?>
-	<p>Usuario:<br> <?= form_input('usuario'); ?><br>
+	<p id="formulario_login">Usuario:<br> <?= form_input('usuario'); ?><br>
 	<br>Contraseña:<br> <?= form_password('pass'); ?></p>
 	<? echo form_submit('Entrar', 'Entrar'); ?><br>
-	<a href=/redsocialpw/>¿Has olvidado tu contraseña?</a>
+	<br>
+	<a id="enlace" href=/redsocialpw/>¿Has olvidado tu contraseña?</a>
 </div>
