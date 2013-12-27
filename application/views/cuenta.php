54a292d8abb41d24926de3ef626f2f670562ab4b
tatyperson22@gmail.com <tatyperson22@gmail.com@15a9a433-5e3c-3676-0093-ef6a8a42323a>
2013-12-27 17:20:46 +0000

    Botones fuera!

--- application/views/cuenta.php
+++ application/views/cuenta.php
@@ -19,20 +19,3 @@
 <div id="avatar"> <? //Si no tiene foto (Hay que comprobarlo en su perfil), se carga una por defecto ?>
 	<img id="logo" src="<?= base_url();?>img/avatar.jpg"/>
 </div>
-
-<div id="primera_accion">
-	<?= form_submit('amigos', 'Amigos'); ?>
-</div>
-
-<div id="segunda_accion">
-	<?= form_submit('actividad', 'Nueva actividad'); ?>
-</div>
-
-<div id="tercera_accion">
-	<?= form_submit('mensajes', 'Mis Mensajes'); ?>
-</div>
-
-<div id="cuarta_accion">
-	<?= form_submit('actividades', 'Mis actividades'); ?>
-</div>
-
