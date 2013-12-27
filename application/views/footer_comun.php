09e452e5b58d78c0d6beca14b77b049bed430751
tatyperson22@gmail.com <tatyperson22@gmail.com@15a9a433-5e3c-3676-0093-ef6a8a42323a>
2013-12-27 16:49:58 +0000

    Puesto el pie en su sitio xD

--- application/views/footer_comun.php
+++ application/views/footer_comun.php
@@ -2,8 +2,8 @@
 <br><br>
 <br><br>
 <br><br>
-<div id="pie">
-	<div id="bloque_centrado">
+<div class="pie bloque_centrado">
+	<div class="bloque_centrado">
 		<p class="fuente_pie">FriendShipDiary&copy; 2014</p>
 		<p><a class="fuente_pie" href="mailto:EmailAdministrador">Contacta con nosotros</a></p>
 	</div>
