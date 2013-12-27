c3a719299dc9c2bee1110451d021bbcc07ed3cdb
tatyperson22@gmail.com <tatyperson22@gmail.com@15a9a433-5e3c-3676-0093-ef6a8a42323a>
2013-12-27 16:52:23 +0000

    Quitado el contacta con nosotros del pie xD

--- application/views/footer_comun.php
+++ application/views/footer_comun.php
@@ -5,7 +5,6 @@
 <div class="pie bloque_centrado">
 	<div class="bloque_centrado">
 		<p class="fuente_pie">FriendShipDiary&copy; 2014</p>
-		<p><a class="fuente_pie" href="mailto:EmailAdministrador">Contacta con nosotros</a></p>
 	</div>
 </div>
 </html>
