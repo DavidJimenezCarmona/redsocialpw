87a47f7a3eb712689d2861901d4ac246be27dad2
tatyperson22@gmail.com <tatyperson22@gmail.com@15a9a433-5e3c-3676-0093-ef6a8a42323a>
2013-12-28 05:24:03 +0000

    Oju, siempre con tonterias

--- application/views/footer_comun.php
+++ application/views/footer_comun.php
@@ -1,7 +1,7 @@
 
 <div class="pie bloque_centrado">
 	<div class="bloque_centrado">
-		<p class="fuente_pie">FriendShipDiary&copy; 2014</p>
+		<p class="fuente_pie_comun">FriendShipDiary&copy; 2014</p>
 	</div>
 </div>
 </body>
