e7156f6b52d2507cc1770c598dba29b02690b0ff
tatyperson22@gmail.com <tatyperson22@gmail.com@15a9a433-5e3c-3676-0093-ef6a8a42323a>
2014-01-02 23:32:00 +0000

    Quitando líneas de código innecesarias!

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -2,10 +2,24 @@
 
 class Welcome extends CI_Controller {
 
-	public function cargarInicio()
-	{
+	function __construct()
+    {
+        parent::__construct(); 
+
+        //Propagamos la sesión
+		if(!isset($_SESSION)) session_start(); 
+
 		$this->load->helper('url');
 		$this->load->helper('form');
+		$this->load->helper('email');
+
+        $this->load->model('modelo_usuario');
+        $this->load->model('modelo_amigo');
+        $this->load->model('modelo_perfil');
+    }
+
+	public function cargarInicio()
+	{
 		$this->load->view('headers');
 		$this->load->view('inicio');
 		$this->load->view('footer');
@@ -13,12 +27,9 @@ class Welcome extends CI_Controller {
 
 	public function cargarCuenta($usuario) 
 	{
-		//Creamos la sesión con la cuenta del usuario
-		session_start();
 		$_SESSION['usuario'] = $usuario;
 
 		//Cargamos el modelo perfil
-		$this->load->model('modelo_perfil');
 		$perfil=$this->modelo_perfil->get_perfil($usuario->id);
 		$_SESSION['perfil'] = $perfil;
 		$this->home();
@@ -27,19 +38,11 @@ class Welcome extends CI_Controller {
 	//Carga la página de inicio
 	public function home() 
 	{
-		if(!isset($_SESSION)) {
-			session_start();
-		}
 		
-		$this->load->helper('url');
-		$this->load->model('modelo_amigo');
 		$notificaciones = $this->modelo_amigo->notificaciones_pendientes($_SESSION['usuario']->id);
 
 		if($notificaciones == 0) {  //Hay notificaciones pendientes
 			$data["notificaciones"]= 0;
-			$this->load->helper('url');
-			$this->load->helper('form');
-			$this->load->library('table');
 			//Cargamos las vistas
 			$this->load->view('headers_cuenta',$data);
 			$this->load->view('cuenta');
@@ -48,9 +51,6 @@ class Welcome extends CI_Controller {
 
 		else {
 			$data["notificaciones"]= 1;
-			$this->load->helper('url');
-			$this->load->helper('form');
-			$this->load->library('table');
 			//Cargamos las vistas
 			$this->load->view('headers_cuenta',$data);
 			$this->load->view('cuenta');
@@ -59,28 +59,36 @@ class Welcome extends CI_Controller {
 	}
 
 	public function modificar_perfil() {
+
+		
 		$this->load->helper('url');
-		$this->load->helper('form');
-		$this->load->library('table');
-		//Cargamos las vistas
-		$this->load->view('headers_cuenta');
-		$this->load->view('modificar_perfil');
-		$this->load->view('footer_comun');
+		$this->load->model('modelo_amigo');
+		$notificaciones = $this->modelo_amigo->notificaciones_pendientes($_SESSION['usuario']->id);
+
+		if($notificaciones == 0) {  //Hay notificaciones pendientes
+			$data["notificaciones"]= 0;
+			//Cargamos las vistas
+			$this->load->view('headers_cuenta', $data);
+			$this->load->view('modificar_perfil');
+			$this->load->view('footer_comun');
+		}
+
+		else {
+			$data["notificaciones"]= 1;
+			//Cargamos las vistas
+			$this->load->view('headers_cuenta', $data);
+			$this->load->view('modificar_perfil');
+			$this->load->view('footer_comun');
+		}
 	}
 
 	public function nuevos_datos_perfil() {
-
-		if(!isset($_SESSION)) {
-			session_start();
-		}
-		
 
 		$perfil['id_ciudad_residencia']=$_POST['id_ciudad_residencia'];
 		$perfil['id_ciudad_nacimiento']=$_POST['id_ciudad_nacimiento'];
 		$perfil['ocupacion']=$_POST['ocupacion'];
 		$perfil['centro_actividad']=$_POST['centro_actividad'];
 
-		$this->load->model('modelo_perfil');
 		$this->modelo_perfil->modificar_perfil($_SESSION['usuario']->id,$perfil);
 
 		echo "Los datos se han actualizado correctamente.";
@@ -90,8 +98,6 @@ class Welcome extends CI_Controller {
 
 	public function cargarInicioErroneo()
 	{
-		$this->load->helper('url');
-		$this->load->helper('form');
 		$this->load->view('headers');
 		$this->load->view('fallo_inicio_sesion');
 		$this->load->view('footer');
@@ -106,15 +112,12 @@ class Welcome extends CI_Controller {
 	public function registro() 
 	{
 		//Cargamos la página de registro
-		$this->load->helper('form');
 		$this->load->view('headers');
 		$this->load->view('nuevoUsuario');
 		$this->load->view('footer');	
 	}
 
 	public function registro_fallido() {
-		$this->load->helper('url');
-		$this->load->helper('form');
 		$this->load->view('headers');
 		$data['mensaje'] = "Los datos no son correctos, por favor complete todos los campos"; //NO HACE FALTA OTRA VISTA SOLO PARA ESO
 		$this->load->view('nuevoUsuario', $data);
@@ -123,12 +126,6 @@ class Welcome extends CI_Controller {
 
 	public function nuevoUsuario()
 	{
-		//Metemos en un array los datos a insertar en la bd
-		$this->load->helper('email');
-
-		//Cargamos el modelo usuario
-		$this->load->model('modelo_usuario');
-
 		if($this->modelo_usuario->unicidad_alias($_POST['alias']) == 1) { //Comprobamos que el alias es único
 
 			$usuario['alias']=$_POST['alias'];
@@ -155,7 +152,6 @@ class Welcome extends CI_Controller {
 				//Llamamos al método del model que crea un nuevo usuario
 				$this->modelo_usuario->insertar_usuario($usuario);
 				//Creamos el perfil asociado (actualmente en blanco) al nuevo usuario
-				$this->load->model('modelo_perfil');
 
 				$data['id_usuario'] = mysql_insert_id();
 				$this->modelo_perfil->insertar_perfil($data);
@@ -179,8 +175,6 @@ class Welcome extends CI_Controller {
 
 	public function login()
 	{
-		$this->load->helper('url');
-		$this->load->model('modelo_usuario');
 		$usuario=$this->modelo_usuario->login($_POST['usuario'], $_POST['pass']);
 
 		if(isset($_POST['registrate']))
@@ -201,8 +195,6 @@ class Welcome extends CI_Controller {
 
 	public function salir()
 	{
-		if(!isset($_SESSION))
-			session_start();
 		session_destroy();
 		$this->cargarInicio();
 	}
